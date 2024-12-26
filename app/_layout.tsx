566bcf076b012b18941f105acab00f5739914d32
Gidy30B <xangida@gmail.com>
2024-12-26 16:45:56 +0300

    added the theme prvider and statusBar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StatusBar as RNStatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './(redux)/store';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 function RootLayout() {
+  const colorScheme = useColorScheme();
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Main Layout</Text>
-    </View>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Main Layout</Text>
+      </View>
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+      <RNStatusBar
+        barStyle={colorScheme === 'dark' ? 'light-content' : 'dark-content'}
+        backgroundColor={colorScheme === 'dark' ? '#000' : '#fff'}
+        translucent={true}
+      />
+    </ThemeProvider>
   );
 }
 
